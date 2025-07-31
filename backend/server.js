7339c772de5e86a409be1c17f53fc7f12c580147
AbdulrahmanChibon <abdulrahmanchibon8@gmail.com>
2025-07-31 20:41:21 +0300

    fix: Simplify CORS configuration for Vercel

--- backend/server.js
+++ backend/server.js
@@ -15,18 +15,7 @@ const app = express();
 mongoose.set("strictQuery", true);
 
 // Middleware
-const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000', 'http://localhost:3001'].filter(Boolean);
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
